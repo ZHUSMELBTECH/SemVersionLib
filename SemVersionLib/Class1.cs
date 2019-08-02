d23cc4ea13605f76ca039ae69e518c46587a11a7
Yu Zhu <devop228@zhusmelb.com>
2019-08-02 16:09:25 +1000

    implement an interface

--- SemVersionLib/Class1.cs
+++ SemVersionLib/Class1.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace SemVersionLib
 {
-    public class Class1
+    public class Class1 : Interface1
     {
+        public int Add(int op1, int op2)
+            => op1 + op2;
+
+        public int Subtract(int op1, int op2)
+            => op1 - op2;
     }
 }
---
+++ SemVersionLib/Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemVersionLib
+{
+    interface Interface1
+    {
+        int Add(int op1, int op2);
+
+        int Subtract(int op1, int op2);
+    }
+}

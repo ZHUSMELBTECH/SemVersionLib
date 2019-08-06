1213b8b313dad6fa418a53a931aa612d10df4c35
Yu Zhu <devop228@zhusmelb.com>
2019-08-06 16:18:03 +1000

    Add nuget spec

--- SemVersionLib/Properties/AssemblyInfo.cs
+++ SemVersionLib/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,13 +7,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SemVersionLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Testing package for nuget versioning")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ZHUSMELBTECH")]
 [assembly: AssemblyProduct("SemVersionLib")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark("ZHUSMELB")]
+[assembly: AssemblyCulture("en-AU")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -21,6 +22,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7763ab69-bac5-43dd-8d0a-41e0f08135ec")]
+[assembly: NeutralResourcesLanguage("en-AU")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +34,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
